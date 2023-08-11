52afd32200dcbc886ac619c43af3a6077cda6440
Yassine <filaliyassine04@gmail.com>
2023-08-11 13:57:18 -0700

    puts 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle);
-    return (0);
+#include <stdio.h>
+int main() {
+	puts("\"Programming is like building a multilingual puzzle);
+	return 0;
 }
